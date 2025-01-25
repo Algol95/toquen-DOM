60b7f594298f16783cfc15a24b541d86c7596ca4
hema <hemapriyaweb@gmail.com>
2025-01-25 01:48:18 +0100

    Added code to instrument.html as well as styles and js. Pending Media Query.

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,1 +1,15 @@
-//Aquí va el código del instrumento
+/* ---------------------------------- KEY VIBRATION ----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+  const keys = document.querySelectorAll('.xylophone__key');
+
+  keys.forEach(key => {
+    key.addEventListener('click', function() {
+      const keyImg = key.querySelector('img');
+      keyImg.classList.add('vibrating');
+      setTimeout(() => {
+        keyImg.classList.remove('vibrating');
+      }, 200);
+    });
+  });
+});
+/* ---------------------------------- END OF CODE ----------------------------------*/
