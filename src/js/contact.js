137b5bb3a1bbafb5f08fce2398d9d42e3dddf3ad
cuyssi <cuyssi@hotmail.com>
2025-02-06 19:44:34 +0100

    Modification of alert message

--- src/js/contact.js
+++ src/js/contact.js
@@ -6,34 +6,44 @@ const form = document.querySelector(".contact__form");
 const confirmationMessage = document.querySelector("#confirmationMessage");
 const contactForm = document.querySelector("#contactForm");
 const closeMessageButton = document.querySelector("#closeMessageButton");
-const alertContainer = document.querySelector("#alertContainer"); // Contenedor de alertas
+const alertContainer = document.querySelector("#alertContainer");
 
-// esta parte escucha al boton y hace varias cosas: si hacemos click primer compruba que los datos esten completos (si no sale una alerta)
+// esta parte escucha al boton y hace varias cosas: si hacemos click primer comprueba que los datos esten completos (si no sale una alerta)
 //si estan completos observa si estan bien rellenados, si no tambien sale una alerta
 //si todo esta correcto activa una animacion de envio y saca la pantalla de agradecimiento, luego la cerramos y nos devuelve al form
+
+alertContainer.style.display = "none";
+
 btn.addEventListener("click", () => {
-   //comprobacion de que los campos esten cubiertos
+   //-------------------------------------------------------------------comprobacion de que los campos esten cubiertos-------------------------------------------------------------------------------
     if (nameInput.value.trim() === "" || phone.value.trim() === "" || mail.value.trim() === "") {
-        alertContainer.innerHTML = `<i class="bi bi-exclamation-triangle"></i> Por favor, completa todos los campos antes de enviar. <i class="bi bi-exclamation-triangle"></i>`;
+        alertContainer.innerHTML = `<p><i class="bi bi-exclamation-triangle"></i> Por favor, completa todos los campos antes de enviar. <i class="bi bi-exclamation-triangle"></i></p>`;
+        alertContainer.style.display = "block";
         return;
+
     }
-    //comprobaciones de que los datos esten correctos
+    //-------------------------------------------------------------------comprobaciones de que los datos esten correctos--------------------------------------------------------------------------------
     if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(nameInput.value)) {
-        alertContainer.innerHTML = `<i class="bi bi-exclamation-triangle"></i> Por favor, introduce un nombre válido (solo letras y espacios). <i class="bi bi-exclamation-triangle"></i>`;
+        alertContainer.innerHTML = `<p><i class="bi bi-exclamation-triangle"></i> Por favor, introduce un nombre válido (solo letras y espacios) <i class="bi bi-exclamation-triangle"></i></p>`;
+        alertContainer.style.display = "block";
         return;
+
     }
     
     if (!/^\d+$/.test(phone.value)) {
-        alertContainer.innerHTML = `<i class="bi bi-exclamation-triangle"></i> Por favor, introduce un número de teléfono válido (solo números). <i class="bi bi-exclamation-triangle"></i>`;
+        alertContainer.innerHTML = `<p><i class="bi bi-exclamation-triangle"></i> Por favor, introduce un número de teléfono válido (solo números) <i class="bi bi-exclamation-triangle"></i></p>`;
+        alertContainer.style.display = "block";
         return;
+
     }
     
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(mail.value)) {
-        alertContainer.innerHTML = `<i class="bi bi-exclamation-triangle"></i> Por favor, introduce un correo electrónico válido. <i class="bi bi-exclamation-triangle"></i>`;
+        alertContainer.innerHTML = `<p><i class="bi bi-exclamation-triangle"></i> Por favor, introduce un correo electrónico válido <i class="bi bi-exclamation-triangle"></i></p>`;
+        alertContainer.style.display = "block";
         return;
-    }
+    }    
 
-    // animación con GSAP al enviar el formulario
+    //--------------------------------------------------------------------------- animación con GSAP al enviar el formulario-------------------------------------------------------------------------------------------
     gsap.to(form, {
         opacity: 0,
         transform: "translateY(-100px)",
@@ -58,9 +68,10 @@ btn.addEventListener("click", () => {
     });
 });
 
-//esto es para que se quite el mensaje de alerta al pinchar otra vez en el input
+//esta parte es para que se quite el mensaje de alerta y su contenedor al pinchar otra vez en el input
 function clearAlert() {
     alertContainer.textContent = "";
+    alertContainer.style.display = "none";
 }
 
 //en esta parte se borra la alerta al hacer click de nuevo
@@ -69,13 +80,13 @@ function clearAlert() {
     input.addEventListener("focus", clearAlert);
 });
 
-// a qui  cerramos manualmente la ventana de confirmación
+// aqui  cerramos manualmente la ventana de confirmación
 closeMessageButton.addEventListener("click", function () {
     gsap.to(confirmationMessage, {
         opacity: 0,
         duration: 0.5,
         onComplete: function () {
-            confirmationMessage.style.display = "none"; // Ocultar el mensaje de confirmación
+            confirmationMessage.style.display = "none"; 
 
             // nos devuelve al form
             gsap.to(form, {
