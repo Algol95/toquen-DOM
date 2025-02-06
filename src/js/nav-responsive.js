cf801a80a6893326c8399d6537b2f4f786262dbb
Algol95 <angelaragon.developer@gmail.com>
2025-02-07 00:23:16 +0100

    Return tu commit 6e875b3

--- src/js/nav-responsive.js
+++ src/js/nav-responsive.js
@@ -13,38 +13,5 @@ close.addEventListener("click", () => {
 })
 
 
-// A partir de aquí comienza el codigo del carrusel:
-const sliderItems = document.querySelectorAll(".slider__item");
-const nextButton = document.querySelector(".content__container__btn--right");
-const backButton = document.querySelector(".content__container__btn--left");
 
-
-let currentIndex = 0;
-
-// Función para mostrar el slide activo
-function showSlide(index) {
-  // Elimina la clase active de todos los slides
-  sliderItems.forEach(slide => slide.classList.remove("active"));
-
-  // Asegura que el índice esté en el rango correcto
-  if (index >= sliderItems.length) {
-    currentIndex = 0;
-  } else if (index < 0) {
-    currentIndex = sliderItems.length - 1;
-  } else {
-    currentIndex = index;
-  }
-
-  // Añade la clase active al slide correspondiente
-  sliderItems[currentIndex].classList.add("active");
-}
-
-// Manejadores de eventos para los botones
-nextButton.addEventListener("click", () => {
-  showSlide(currentIndex + 1);
-});
-
-backButton.addEventListener("click", () => {
-  showSlide(currentIndex - 1);
-});
 
