4402f76b78c5d23a11f316215ddddd7b5dc86600
Algol95 <angelaragon.developer@gmail.com>
2025-02-07 01:29:37 +0100

    Fixed nav buged, fixed scroll-x and nav links updated

--- src/js/nav-responsive.js
+++ src/js/nav-responsive.js
@@ -2,6 +2,8 @@
 const nav = document.getElementById("nav");
 const open = document.getElementById("open");
 const close = document.getElementById("close");
+// Recojo ID de la nav instruments para arreglar el bug TED94 - AMCA
+const navInstr = document.getElementById("nav-instruments");
 
 
 open.addEventListener("click", () => {
@@ -12,6 +14,9 @@ close.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
 
-
+// Elimina la clase .visible cuando hacemos click en el nav para viajar a section instruments - AMCA
+navInstr.addEventListener("click", () => {
+  nav.classList.remove("visible");
+})
 
 
